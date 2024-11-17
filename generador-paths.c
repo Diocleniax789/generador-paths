00b523a0b56e7212a4ff8801a2e6ab703d7363b0
Diocleniax789 <julioramdom123@gmail.com>
2024-11-17 00:26:43 -0300

    se ha completado la funcion para validar la ruta

--- generador-paths.c
+++ generador-paths.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <windows.h>
+
 
 struct path{
     char ruta[128];
@@ -66,3 +68,62 @@ char * cargaRuta(){
     printf("\n - Path generado - \n");
     return ruta;
 }
+
+int validaRuta(char *ruta){
+    int longitud_path,i,flag = 0,longitud_token;
+    char delim[] = "/", *token;
+
+    longitud_path = strlen(ruta);
+    if(longitud_path >= 3 && longitud_path <= 128){
+        printf("\n * LONGITUD VALIDA * \n");
+        if(ruta[0] == '/' && ruta[longitud_path - 1] == '/'){
+
+            token = strtok(ruta,delim);
+            while(token != NULL){
+                longitud_token = strlen(token);
+                for(i = 0; i < longitud_token; i++){
+                    if(isalnum(token[i]) != 0){
+                        continue;
+                    } else{
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if(flag == 0){
+                    Sleep(1000);
+                    printf("\n * DIRECTORIO CONFORMADO POR CARACTERES ALFANUMERICOS * \n");
+                    if(longitud_token >= 1 && longitud_token <= 8){
+                            Sleep(1000);
+                            printf("\n * DIRECTORIO VALIDO * \n");
+                            return 0;
+                    } else{
+                            Sleep(1000);
+                            printf("\n x LONGITUD SOBREPASA LOS 8 CARACTERES x \n");
+                            return 1;
+                    }
+                } else{
+                    Sleep(1000);
+                    printf("\n x SE HAN DETECTADO CARACTERES NO ALFANUMERICOS x \n");
+                    break;
+                    return 1;
+                }
+
+                token = strtok(NULL,ruta);
+            }
+
+        } else{
+            Sleep(1000);
+            printf("\n x LA RUTA NO COMIENZA CON O TERMINA CON '/' x \n");
+            return 1;
+        }
+
+    } else{
+        Sleep(1000);
+        printf("\n x LONGITUD INVALIDA x \n");
+        return 1;
+    }
+
+}
+
+
